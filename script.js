bede608a784aa03796f718706a42f8406b3a7bc4
Jinju Loeding <jloeding@originate.ie>
2021-06-14 11:34:53 +0100

    Back to basic working

--- script.js
+++ script.js
@@ -31,42 +31,22 @@ gapi.analytics.ready(function() {
   // Render the view selector to the page.
   viewSelector.execute();
 
-  /**
-   * Create a table chart showing top browsers for users to interact with.
-   * Clicking on a row in the table will update a second timeline chart with
-   * data from the selected browser.
-   */
-  var mainChart = new gapi.analytics.googleCharts.DataChart({
-    query: {
-      'dimensions': 'ga:browser',
-      'metrics': 'ga:sessions',
-      'sort': '-ga:sessions',
-      'max-results': '6'
-    },
-    chart: {
-      type: 'TABLE',
-      container: 'main-chart-container',
-      options: {
-        width: '100%'
-      }
-    }
-  });
-
 
   /**
-   * Create a timeline chart showing sessions over time for the browser the
-   * user selected in the main chart.
+   * Create a new DataChart instance with the given query parameters
+   * and Google chart options. It will be rendered inside an element
+   * with the id "chart-container".
    */
-  var breakdownChart = new gapi.analytics.googleCharts.DataChart({
+  var dataChart = new gapi.analytics.googleCharts.DataChart({
     query: {
-      'dimensions': 'ga:date',
-      'metrics': 'ga:sessions',
-      'start-date': '7daysAgo',
+      metrics: 'ga:sessions',
+      dimensions: 'ga:date',
+      'start-date': '30daysAgo',
       'end-date': 'yesterday'
     },
     chart: {
+      container: 'chart-container',
       type: 'LINE',
-      container: 'breakdown-chart-container',
       options: {
         width: '100%'
       }
@@ -75,66 +55,10 @@ gapi.analytics.ready(function() {
 
 
   /**
-   * Store a refernce to the row click listener variable so it can be
-   * removed later to prevent leaking memory when the chart instance is
-   * replaced.
-   */
-  var mainChartRowClickListener;
-
-
-  /**
-   * Update both charts whenever the selected view changes.
+   * Render the dataChart on the page whenever a new view is selected.
    */
   viewSelector.on('change', function(ids) {
-    var options = {query: {ids: ids}};
-
-    // Clean up any event listeners registered on the main chart before
-    // rendering a new one.
-    if (mainChartRowClickListener) {
-      google.visualization.events.removeListener(mainChartRowClickListener);
-    }
-
-    mainChart.set(options).execute();
-    breakdownChart.set(options);
-
-    // Only render the breakdown chart if a browser filter has been set.
-    if (breakdownChart.get().query.filters) breakdownChart.execute();
-  });
-
-
-  /**
-   * Each time the main chart is rendered, add an event listener to it so
-   * that when the user clicks on a row, the line chart is updated with
-   * the data from the browser in the clicked row.
-   */
-  mainChart.on('success', function(response) {
-
-    var chart = response.chart;
-    var dataTable = response.dataTable;
-
-    // Store a reference to this listener so it can be cleaned up later.
-    mainChartRowClickListener = google.visualization.events
-        .addListener(chart, 'select', function(event) {
-
-      // When you unselect a row, the "select" event still fires
-      // but the selection is empty. Ignore that case.
-      if (!chart.getSelection().length) return;
-
-      var row =  chart.getSelection()[0].row;
-      var browser =  dataTable.getValue(row, 0);
-      var options = {
-        query: {
-          filters: 'ga:browser==' + browser
-        },
-        chart: {
-          options: {
-            title: browser
-          }
-        }
-      };
-
-      breakdownChart.set(options).execute();
-    });
+    dataChart.set({query: {ids: ids}}).execute();
   });
 
 });
