76a9e1d8470f8649f5713172e87ea064e803d246
Jinju Loeding <jloeding@originate.ie>
2021-06-15 09:56:15 +0100

    testing

---
+++ assets/scripts/variables.js
@@ -0,0 +1,15 @@
+/****************************
+**** Client Keys and IDs ****
+****************************/
+
+// Analytics View ID, format ga:XXXXXXXXX
+const analyticsViewID = 'ga:104934590';
+
+// OAuth Client ID found in Developer Console
+const developerClientID = '862258600110-jkk79eng4i9rpldoi6l8pj8t576b6gju.apps.googleusercontent.com';
+
+
+/****************************
+****** Custom Branding ******
+****************************/
+
