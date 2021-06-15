69875aeb3da68e3fe292e0f1f5cf12baccf1fd9c
Jinju Loeding <jloeding@originate.ie>
2021-06-15 08:59:21 +0100

    testing

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const developerClientID = '862258600110-jkk79eng4i9rpldoi6l8pj8t576b6gju.apps.go
 // Begin Analytics Embed
 gapi.analytics.ready(function() {
 
-  setTimeout(checkAuth, 4000);
-  function checkAuth() {
-  if (gapi.analytics.auth.isAuthorized() == false ){
-      document.getElementById('main').style.display = "none";
-  } else {
-    document.getElementById('main').style.display = "block";
-  }
+  setTimeout(checkAuth, 2000);
+    function checkAuth() {
+    if (gapi.analytics.auth.isAuthorized() == true ){
+      document.getElementById('main').style.display = "block";
+    } else {
+    document.getElementById('main').style.display = "none";
+    }
+  
+  
+  
   }
   /**
    * Authorize the user immediately if the user has already granted access.
