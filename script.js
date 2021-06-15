9965e13f8243747189bdc1582cfe4da466c87ad4
Jinju Loeding <jloeding@originate.ie>
2021-06-15 09:04:16 +0100

    testing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const developerClientID = '862258600110-jkk79eng4i9rpldoi6l8pj8t576b6gju.apps.go
 // Begin Analytics Embed
 gapi.analytics.ready(function() {
 
-  setTimeout(checkAuth, 2000);
+  setTimeout(checkAuth, 4000);
     function checkAuth() {
     if (gapi.analytics.auth.isAuthorized() == true ){
       document.getElementById('main').style.display = "block";
